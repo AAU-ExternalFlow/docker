5450021cdef6877a5d1d60124e75265e08283226
ScooterHenning <victorhm2000@gmail.com>
2023-05-08 21:42:28 +0200

    DashWebsite iteration 1
    Image upload and display

--- app/app.py
+++ app/app.py
@@ -1,33 +1,78 @@
 import os
+import datetime
 from dash import Dash, html, dcc
+from dash.dependencies import Input, Output, State
 import plotly.express as px
 import pandas as pd
 
 debug = False if os.environ["DASH_DEBUG_MODE"] == "False" else True
 
-app = Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+# app = Dash(__name__)
 
 server = app.server
 
-data = pd.DataFrame(
-    {
-        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-        "Amount": [4, 1, 2, 2, 4, 5],
-        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
-    }
-)
+# data = pd.DataFrame(
+#     {
+#         "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#         "Amount": [4, 1, 2, 2, 4, 5],
+#         "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+#     }
+# )
 
-graph = px.bar(data, x="Fruit", y="Amount", color="City", barmode="group")
+# graph = px.bar(data, x="Fruit", y="Amount", color="City", barmode="group")
 
-app.layout = html.Div(
-    children=[
-        html.H1(
-            children=f"AAU Energy External Flow"
-        ),
-        html.Div(children="""Dash: A web application framework for your data."""),
-        dcc.Graph(id="example-graph", figure=graph),
-    ]
-)
+app.layout = html.Div([
+    dcc.Upload(
+        id='upload-image',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-image-upload'),
+])
+
+def parse_contents(contents, filename, date):
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        # HTML images accept base64 encoded strings in the same format
+        # that is supplied by the upload
+        html.Img(src=contents),
+        html.Hr(),
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+@app.callback(Output('output-image-upload', 'children'),
+              Input('upload-image', 'contents'),
+              State('upload-image', 'filename'),
+              State('upload-image', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
 
 
 if __name__ == "__main__":
